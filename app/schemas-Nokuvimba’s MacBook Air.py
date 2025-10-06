245c1c7c40176c3b3e3ae7135688b8b954838915
Nokuvimba <G00423951@atu.ie>
2025-10-06 21:17:48 +0100

    deleted an extra schema.py file

--- app/schemas-Nokuvimba’s MacBook Air.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic import BaseModel, EmailStr, constr, conint
-
-class User(BaseModel):
-    user_id: int
-    name: constr(min_length=2, max_length=50)
-    email: EmailStr
-    age: conint(gt=18)
-    student_id : constr(min_length)
