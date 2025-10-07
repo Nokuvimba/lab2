376042150919161639c1f05819473f04f1935492
Nokuvimba <G00423951@atu.ie>
2025-10-07 19:37:32 +0100

    added the put test with comments

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def update_user(user_id: int, user: User):
         if u.user_id == user_id:
             users[i] = user # update user in the in-memory list
             return user
-    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user_id not found")
 
 @app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT) # delete user by user_id
 def delete_user(user_id: int):
--- tests/test_users.py
+++ tests/test_users.py
@@ -33,3 +33,26 @@ def test_delete_then_404(client):
  assert r1.status_code == 204
  r2 = client.delete("/api/users/10")
  assert r2.status_code == 404
+
+def test_update_user_then_404(client):
+    # Creating user with id 11 
+    r_create = client.post("/api/users", json=user_payload(uid=11))
+    assert r_create.status_code == 201 #check if it was created 
+
+    # ensuring that the user_id in path and body are the same
+    updated_user = user_payload(uid=999)  #ignored by endpoint because it's not 11
+    updated_user["name"] = "Updated Name"
+
+    #user's name with id 11 is updated
+    r1 = client.put("/api/users/11", json=updated_user) 
+    assert r1.status_code == 202 #Used 202 because module 'starlette.status' has no attribute 'HTTP_200_SUCCESS'
+    data = r1.json()
+    assert data["user_id"] == 11
+    assert data["name"] == "Updated Name"
+
+    # updating a non-existing user
+    r2 = client.put("/api/users/999", json=updated_user)
+    assert r2.status_code == 404
+ 
+ 
+ 
